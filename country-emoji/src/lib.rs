da7b35018be537985a23019c9d47575079e4b76d
sqlerrorthing <148702857+sqlerrorthing@users.noreply.github.com>
2025-05-31 20:31:53 +0800

    use in ipinfo

--- country-emoji/src/lib.rs
+++ country-emoji/src/lib.rs
@@ -8,7 +8,7 @@ const FLAG_MAGIC_NUMBER: u32 = 0x1F1E6 /* 🇦 */ - 'A' as u32;
 
 pub fn internal_code_to_flag<'a>(code: &str) -> Option<String> {
     let mut flag = String::new();
-    
+
     for ch in code.trim().to_uppercase().chars() {
         if let Some(c) = char::from_u32(ch as u32 + FLAG_MAGIC_NUMBER) {
             flag.push(c);
